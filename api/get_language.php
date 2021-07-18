dc9198ec34e713eab891b3c560a4cc7954448228
hamedpro <hamedyaghootpour@gmail.com>
2021-07-18 12:23:28 +0430

    rewrite codebase - switching to electronjs app for desktop

--- api/get_language.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("db.php");
-function get_language(){
-    global $db;
-    $sql = "select * from main where key_name = 'language'";
-    $language = mysqli_fetch_assoc($db->query($sql))["key_value"];
-    return $language;
-}
-echo get_language();
--- api/localStorageApi.js
+++ api/localStorageApi.js
@@ -1,18 +1,4 @@
-var api = {}
-api.new_report = function(driver_code,driver_name,report_text,is_open){
-    id = 1
-    if(api.get_reports().length != 0){
-        id = (api.get_reports().length +1)
-    }
-    new_report = {
-        driver_code,
-        driver_name,
-        report_text,
-        is_open,
-        id
-    }
-    appendToLocalStorageArray('reports',new_report)
-}
+
 api.get_reports = function(){
     if(localStorage.getItem('reports') != null){
         return JSON.parse(localStorage.getItem('reports'))
