a0f0bdb4c17dc19f3ef76b04426ef0402d9b27a0
hamedpro <hamedyaghootpour@gmail.com>
2021-08-05 15:56:43 +0430

    change lang to persian

--- src/components/new_report/NewReport.js
+++ src/components/new_report/NewReport.js
@@ -1,4 +1,5 @@
 import "./styles.css"
+import "../../common/common.css"
 import { Component } from "react"
 var api = require('../../browser_api')
 
@@ -26,22 +27,22 @@ class NewReport extends Component{
 		return (
 			<div id="new-report">
 				<div className="container-fluid">
-					<div className="row mt-2">
-						<h1 className="text-primary">new_report</h1>
+					<div className="row mt-2 dir-rtl">
+						<h1 className="text-primary">ثبت گزارش جدید</h1>
 					</div>
 					<hr/>
-					<div className="row mb-1">
+					<div className="row mb-1 dir-rtl">
 						<div className="col-8">
-							<h4 className="text-info">enter driver code: </h4>
+							<h4 className="text-info">کد راننده مورد نظر :</h4>
 						</div>
 						<div className="col">
 							<input type="number" className="form-control text-light bg-dark border-0" min="0" id="driver_code"/>
 						</div>
 					</div>
 					<hr/>
-					<div className="row mb-2">
-						<div className="col-5">
-							<h4 className="text-info">driver name: </h4>
+					<div className="row mb-2 dir-rtl">
+						<div className="col-6">
+							<h4 className="text-info">نام کامل راننده :</h4>
 						</div>
 						<div className="col">
 							<input className="form-control text-light bg-dark border-0" type="text" id="driver_name"/>
@@ -49,25 +50,25 @@ class NewReport extends Component{
 						
 					</div>
 					<hr/>
-					<div className="row mb-3">
+					<div className="row mb-3 dir-rtl">
 						
-						<h4 className="text-info">enter report text:</h4>
+						<h4 className="text-info">متن گزارش را وارد کنید :</h4>
 						
 					</div>
 					
 					<div className="row d-flex justify-content-center">
 						<div className="col-12">
-							<textarea className="form-control text-light bg-dark border-0" id="report_text"></textarea>
+							<textarea className="form-control text-light bg-dark border-0 dir-rtl" id="report_text"></textarea>
 						</div>
 						
 					</div>
 					
 					<div className="row mt-3 mb-3">
 						<div className="col-8 d-grid">
-							<button className="btn btn-primary" id="add_new_report" onClick={this.add_new_report}>save report</button>
+							<button className="btn btn-primary" id="add_new_report" onClick={this.add_new_report}>ثبت نهایی گزارش</button>
 						</div>
 						<div className="col-4 d-flex justify-content-end">
-							<button className="btn btn-outline-danger" id="discard" onClick={this.discard}>discard</button>
+							<button className="btn btn-outline-danger" id="discard" onClick={this.discard}>بی خیال</button>
 						</div>
 					</div>
 				</div>
--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,6 +1,8 @@
 import { Component } from "react"
 import "./styles.css"
+import "../../common/common.css"
 var api = require('../../browser_api')
+
 
 class Settings extends Component{
 	reset_factory(){
@@ -16,14 +18,14 @@ class Settings extends Component{
 		return (
 			<div id="settings">
 				<div className="container-fluid">
-					<div className="row mt-2">
+					<div className="row mt-2 dir-rtl">
 						<h1 className="text-primary">settings</h1>
 					</div>
 				</div>
 				<hr/>
-				<div className="row mb-2 mx-1">
+				<div className="row mb-2 mx-1 dir-rtl">
 					<div className="col-6">
-						<button className="btn btn-info" id="reset_factory" onClick={this.reset_factory}>reset factory</button>
+						<button className="btn btn-info" id="reset_factory" onClick={this.reset_factory}>بازنشانی کارخانه</button>
 					</div>
 				</div>
 				
